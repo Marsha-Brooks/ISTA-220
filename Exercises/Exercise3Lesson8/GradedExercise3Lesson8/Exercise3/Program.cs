043e53e3d7b19108e02033cd031874154a3e89bc
Marsha J. Brooks <31009339+Marsha-Brooks@users.noreply.github.com>
2017-08-22 11:57:43 -0400

    Add files via upload

---
+++ Exercises/Exercise3Lesson8/GradedExercise3Lesson8/Exercise3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            string number;
+            double total = 0;
+            int numbers = 0;
+            double average;
+            Console.WriteLine("Enter all of your Test scores, hit enter twice when complete");
+            while ((number = Console.ReadLine()) != "")
+            {
+                total += double.Parse(number);
+                numbers++;
+            }
+            Console.WriteLine("The average of the numbers entered is {0}", total / numbers);
+            average = total / numbers;
+            DetermineGrade(average);
+        }
+
+            public static void DetermineGrade(double avg)
+        {
+            char letterGrade = ' ';
+
+            if (avg > 89)
+                letterGrade = 'A';
+            else
+                if (avg > 79)
+                letterGrade = 'B';
+            else
+        if (avg > 69)
+                letterGrade = 'C';
+            else
+        if (avg > 59)
+                letterGrade = 'D';
+            else
+ if (avg <= 59)
+                letterGrade = 'F';
+
+            Console.WriteLine(" Your Average Letter Grade is: " + letterGrade + " Your Average Score is: " + avg);
+            Console.ReadKey();
+        }
+    }
+}
+
