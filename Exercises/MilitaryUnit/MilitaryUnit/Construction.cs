cf19987d58ddc576559d34a248212bfcdbd14ccd
Marsha J. Brooks <31009339+Marsha-Brooks@users.noreply.github.com>
2017-09-04 12:16:32 -0400

    Add files via upload

---
+++ Exercises/MilitaryUnit/MilitaryUnit/Construction.cs
@@ -0,0 +1,31 @@
+﻿using MilitaryUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryUnit
+{
+    class Construction: Engineers
+    {
+        public void HorizVert()
+        {
+            Console.WriteLine("We push it down or build it up!");
+        }
+
+        public void ConstEquip()
+        {
+            Console.WriteLine("Dozers, Rollers, FrontLoaders and TNT, Big Toys!");
+        }
+
+        public override void Mission()
+        {
+            Console.WriteLine("Make a way for support elements to get through and build up footprint for operations");
+        }
+        
+
+    }
+    }
+
+
---
+++ Exercises/MilitaryUnit/MilitaryUnit/Signal.cs
@@ -0,0 +1,33 @@
+﻿using MilitaryUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryUnit
+{
+    class Signal : Engineers
+    
+        {
+            public void AppendixH()
+            {
+                Console.WriteLine("Who are we talking to and how?");
+            }
+
+            public void CommoShop()
+            {
+                Console.WriteLine("Accountable for all secure and nosecure commo equip");
+            }
+
+            public override void Mission()
+            {
+                Console.WriteLine("Deploy redundant forms of communication across virtual battle field");
+            }
+        
+    }
+    }
+
+
+
+
