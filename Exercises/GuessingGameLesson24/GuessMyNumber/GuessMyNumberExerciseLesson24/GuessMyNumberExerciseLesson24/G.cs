7a7674734d2730c1e89ea7f740a7779766d48f9f
Marsha J. Brooks <31009339+Marsha-Brooks@users.noreply.github.com>
2017-09-16 19:16:20 -0400

    Add files via upload

---
+++ Exercises/GuessingGameLesson24/GuessMyNumber/GuessMyNumberExerciseLesson24/GuessMyNumberExerciseLesson24/G.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumberExerciseLesson24
+{
+    class Bisection
+    {
+        public static int selectNumber = 0;
+        public static Random Rand = new Random();
+        public static bool GameOver = false;
+
+        public static void Main(string[] args)
+        {
+            selectNumber = Rand.Next(1, 101);
+            int userNumber = 0;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 100");
+                userNumber = Convert.ToInt32(Console.ReadLine());
+                GuessMyNumber(userNumber);
+
+            }
+            while (GameOver == false);
+            {
+                Console.ReadLine();
+
+            }
+        }
+            public static void GuessMyNumber(int userNumber)
+            {
+                int playAgian = 0;
+                if (userNumber < selectNumber)
+                {
+                    Console.WriteLine("Too low, Try Agian");
+                }
+                else if (userNumber > selectNumber)
+                {
+                    Console.WriteLine("To high, Try Agian");
+                }
+                else
+                {
+                    Console.WriteLine("You Got It! Press 1 to Play Agian or 2 to Quit");
+                    playAgian = Convert.ToInt32(Console.ReadLine());
+                    while (playAgian != 1 && playAgian != 2)
+                    {
+                        Console.WriteLine("Please select a correct response, choose 1 to PlayAgian or 2 to quit");
+                    }
+                    if (playAgian == 2)
+
+                        GameOver = true;
+
+                    else
+
+                        selectNumber = Rand.Next(1, 101);
+
+                }
+            }
+
+        }
+    }
+
---
+++ Exercises/GuessingGameLesson24/GuessMyNumber/GuessMyNumberLesson24Pt2/GuessMyNumberLesson24Pt2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumberLesson24Pt2
+{
+    class Program
+    {
+        public static int selectNumber = 0;
+        public static Random Rand = new Random();
+        public static bool GameOver = false;
+        public static int userInput = 0;
+        public static string guess = "";
+        public static int x0 = 0;
+        public static int x1 = 100;
+        public static int x2 = 50;
+        public static int count = 1;
+        public static string guesses = "";
+
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Guess a number between 1 and 100 shhh don't tell me, let me guess.");
+
+
+            do
+            {
+                selectNumber = Rand.Next(x0, x1);
+                guess = selectNumber + "";
+                Console.WriteLine("Computer guesses" + " " + guess + '\n' + "If this is Correct: press 1" + 
+                    '\n' + "If the guess is too High: press 2" + '\n' + 
+                   "If the guess is too Low: press 3");
+
+                userInput = Convert.ToInt32(Console.ReadLine());
+                count = count + 1;
+                guesses = count + "";
+             
+
+
+                
+                
+
+                GuessYourNumber(userInput);
+             
+                    
+
+            }
+            while (GameOver == false);
+            {
+                Console.ReadLine();
+
+            }
+        }
+        public static void GuessYourNumber(int userInput)
+
+        {
+            int playAgian = 0;
+
+            if (userInput == 2)
+            {
+                x2 = selectNumber;
+                x1 = x2;
+                selectNumber = Rand.Next(x0, x1);
+                Console.WriteLine("Dang it! I will have to guess for the" + " " + guesses + " " + " time because you said:");
+                Console.WriteLine("I guessed too High!");
+            }
+            else if (userInput == 3)
+            {
+                x2 = selectNumber;
+                x0 = x2;
+                selectNumber = Rand.Next(x0, x1);
+                Console.WriteLine("Dang it! I will have to guess for the" + " " + guesses + " " + " time because you said:");
+                Console.WriteLine("I guessed to Low!");
+
+
+            }
+            else
+            
+            {
+                Console.WriteLine("I got it right?! Awesome Sauce!"  + '\n' + "If you want to play agian Press:  4" + '\n'+ "If you want to quit Press: 5");
+                playAgian = Convert.ToInt32(Console.ReadLine());
+               
+                if (playAgian == 5)
+                {
+                    GameOver = true;
+                }
+                else if (playAgian == 4 )
+                {
+                    Console.WriteLine("Guess a number between 1 and 100 shhh don't tell me");
+                }
+            }
+        }
+            
+
+
+
+            }
+        }
+
+    
+
+
